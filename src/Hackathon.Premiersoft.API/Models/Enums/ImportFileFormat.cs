3bd73647c4afe0db9c27738e37eb6ca203565e81
Breno Van Dall <vbreno335@gmail.com>
2025-09-20 14:52:13 -0300

    feats

---
+++ src/Hackathon.Premiersoft.API/Models/Enums/ImportFileFormat.cs
@@ -0,0 +1,12 @@
+﻿namespace Hackathon.Premiersoft.API.Models.Enums
+{
+    public enum ImportFileFormat
+    {
+        Excel,
+        Csv,
+        Xml,
+        Json,
+        Hl7,
+        Fhir
+    }
+}
---
+++ src/Hackathon.Premiersoft.API/Models/Enums/ImportStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Hackathon.Premiersoft.API.Models.Enums
+{
+    public enum ImportStatus
+    {
+        Done = 0,
+        DoneWithWarnings,
+        Fail,
+        Processing
+    }
+}
