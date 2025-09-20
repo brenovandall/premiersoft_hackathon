51148f1a0d44b49433b5719cd55647541b1cc2ed
snortexware <lucasmoreiraprivado@proton.me>
2025-09-20 13:17:24 -0300

    Criação de entidades

---
+++ src/Hackathon.Premiersoft.API/Models/Hospitais.cs
@@ -0,0 +1,13 @@
+﻿using Hackathon.Premiersoft.API.Models.Abstractions;
+
+namespace Hackathon.Premiersoft.API.Models
+{
+    public class Hospitais : Entity<int>
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Bairro { get; set; }
+        public Municipios Cidade { get; set; }
+        public long Leitos_totais { get; set; }
+    }
+}
---
+++ src/Hackathon.Premiersoft.API/Models/Pacientes.cs
@@ -0,0 +1,16 @@
+﻿using Hackathon.Premiersoft.API.Models.Abstractions;
+
+namespace Hackathon.Premiersoft.API.Models
+{
+    public class Pacientes : Entity<int>
+    {
+        public string Codigo { get; set; }
+        public string Cpf { get; set; }
+        public string Genero { get; set; }
+        public string Nome_completo { get; set; }
+        public Municipios Codigo_Municipio { get; set; }
+        public bool Convenio { get; set; }
+        public Cid10 Cid10 { get; set; }
+
+    }
+}
