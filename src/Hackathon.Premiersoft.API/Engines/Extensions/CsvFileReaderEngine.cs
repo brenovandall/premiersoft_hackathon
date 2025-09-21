4f9eab820b65410ea72f0f7603e5f308d6bbfa39
junior de oliveira belem <85002295+JuniorOliveiraj@users.noreply.github.com>
2025-09-20 22:53:52 -0300

    Correção erro Build @Lucas

--- src/Hackathon.Premiersoft.API/Engines/Extensions/CsvFileReaderEngine.cs
+++ src/Hackathon.Premiersoft.API/Engines/Extensions/CsvFileReaderEngine.cs
@@ -1,17 +1,13 @@
-﻿using Hackathon.Premiersoft.API.Dto;
-using Hackathon.Premiersoft.API.Engines.Csv;
+﻿using Hackathon.Premiersoft.API.Engines.Csv;
 using Hackathon.Premiersoft.API.Services;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using System.Globalization;
-using System.IO;
-using System.Text;
+using System;
+using System.Threading.Tasks;
 
 namespace Hackathon.Premiersoft.API.Engines.Extensions
 {
     public class CsvFileReaderEngine : Factory.IFileReaderEngine
     {
-
-        public string FileReaderProvider => throw new NotImplementedException();
+        public string FileReaderProvider => "CsvFileReader";
 
         public void Run(long importId)
         {
@@ -28,22 +24,7 @@ namespace Hackathon.Premiersoft.API.Engines.Extensions
                 {
                     Console.WriteLine($"Erro ao processar arquivo em background: {ex.Message}");
                 }
-            };
-        }
-
-        private string NormalizeHeaderName(string headerName)
-        {
-            return headerName
-                .Replace(" ", "_")
-                .Replace("-", "_")
-                .ToLowerInvariant();
-        }
-        private List<CsvRow> ParseRows(string[] dataLines, List<CsvHeader> headers)
-        {
-            // Este método não é mais usado com a versão streaming, pode ser removido se quiser
-            throw new NotImplementedException("Use ParseCsvDataAsync para leitura por stream.");
             });
         }
     }
-
 }
